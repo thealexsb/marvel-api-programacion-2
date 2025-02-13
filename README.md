aa4a62211a5b6367807944389ed964a095b6a1f6
thealexsb <gamersulxxx@gmail.com>
2025-02-13 11:23:03 -0400

    Ajustado para Presentar

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # marvelAPI
+
+
+# Importante Ver Este Video
+
+https://www.youtube.com/watch?v=T_FyWCPOLIc
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const marvel = {
   render: () => {
-    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=XXX&hash=XXX';
+    const urlAPI = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d64bc8731bec0d3fe9c47a1dc441461b&hash=7a1276a200a25d38374fbb078498e5e3';
     const container = document.querySelector('#marvel-row');
     let contentHTML = '';
 
